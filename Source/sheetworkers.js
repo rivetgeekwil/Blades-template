3bc933b8283771d5c9efd8213f135e0a881513bd
rivetgeekwil <rivet.geek.dba@gmail.com>
2021-12-30 18:15:13 -0800

    Cell Special abilities

--- Source/sheetworkers.js
+++ Source/sheetworkers.js
@@ -11,21 +11,21 @@ Object.keys(data.crew).forEach(crew => {
 			base[attr] = getTranslation(base[attr]);
 		}
 	});
-	/* Generate crew contacts from translation file */
-	data.crew[crew].contact = [...Array(data.maxContactsPerCrew).keys()].map(i => ({
-		name: getTranslation(`crew_${crew}_contact_${i}`)
-	}));
+	// /* Generate crew contacts from translation file */
+	// data.crew[crew].contact = [...Array(data.maxContactsPerCrew).keys()].map(i => ({
+	// 	name: getTranslation(`crew_${crew}_contact_${i}`)
+	// }));
 	data.crew[crew].crewability = data.crew[crew].crewability.map(name => ({
 		name: getTranslation(`crew_ability_${name}`),
 		description: getTranslation(`crew_ability_${name}_description`)
 	}));
-	data.crew[crew].upgrade.forEach(upgrade => {
-		upgrade.name = getTranslation(upgrade.name);
-		if (upgrade.description) {
-			upgrade.description = getTranslationByKey(upgrade.description) || "";
-		}
-		upgrade.boxes_chosen = "1";
-	});
+	// data.crew[crew].upgrade.forEach(upgrade => {
+	// 	upgrade.name = getTranslation(upgrade.name);
+	// 	if (upgrade.description) {
+	// 		upgrade.description = getTranslationByKey(upgrade.description) || "";
+	// 	}
+	// 	upgrade.boxes_chosen = "1";
+	// });
 });
 data.items.forEach(item => {
 	item.boxes_chosen = "1";
